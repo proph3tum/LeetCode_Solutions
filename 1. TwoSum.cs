628774f6a581e7aa87e2e83d5d4e282f9b99ea0d
Никита <70805747+proph3tum@users.noreply.github.com>
2022-09-29 15:40:42 +0700

    two solutions

---
+++ 1. TwoSum.cs
@@ -0,0 +1,23 @@
+﻿// https://leetcode.com/problems/two-sum/
+
+namespace LeetCodeSolution;
+
+public partial class Solution
+{
+	public static int[] TwoSum(int[] nums, int target)
+	{
+		for (int i = 0; i < nums.Length; i++)
+		{
+			for (int j = i + 1; j < nums.Length; j++)
+			{
+				int sum = nums[i] + nums[j];
+				if (sum == target)
+				{
+					return new int[] { i, j };
+				}
+			}
+		}
+
+		return Array.Empty<int>();
+	}
+}
---
+++ 1480. RunningSum.cs
@@ -0,0 +1,19 @@
+﻿// https://leetcode.com/problems/running-sum-of-1d-array/
+
+namespace LeetCodeSolution;
+
+public partial class Solution
+{
+	public static int[] RunningSum(int[] nums)
+	{
+		var sum = 0;
+		var array = new int[nums.Length];
+		for (var i = 0; i < nums.Length; i++)
+		{
+			sum += nums[i];
+			array[i] = sum;
+		}
+
+		return array;
+	}
+}
