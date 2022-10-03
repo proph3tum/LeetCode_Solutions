cf1e644d417ef6de5a8243a40804d17cc2bc499b
Никита <70805747+proph3tum@users.noreply.github.com>
2022-10-03 16:16:23 +0700

    1672. RichestCustomerWealth solution

---
+++ 1672. RichestCustomerWealth.cs
@@ -0,0 +1,28 @@
+﻿namespace LeetCodeSolution;
+
+public static class RichestCustomerWealth
+{
+	private static int MaxValue(int[][] accounts)
+	{
+		var res = 0;
+		for (var i = 0; i < accounts.Length; i++)
+		{
+			var temporary = 0;
+			for (var j = 0; j < accounts[i].Length; j++)
+			{
+				temporary += accounts[i][j];
+			}
+
+			res = Math.Max(res, temporary);
+		}
+
+		return res;
+		// return accounts.Max(x => x.Sum());
+	}
+
+	private static void Main()
+	{
+		int[][] accounts = { new int[] { 1, 2, 3 }, new[] { 3, 2, 1 } };
+		Console.WriteLine(MaxValue(accounts));
+	}
+}
