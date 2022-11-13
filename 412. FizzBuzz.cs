b5782418732b030aa216e546bc721aa2720430ee
Никита <70805747+proph3tum@users.noreply.github.com>
2022-11-13 22:05:06 +0700

    Solved exercise number 412
    https://leetcode.com/problems/fizz-buzz/description/

---
+++ 412. FizzBuzz.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCode_Solutions;
+
+public class LeetCode
+{
+    // https://leetcode.com/problems/fizz-buzz/description/
+    public static string[] FizzBuzz(int n)
+    {
+        var answerInt = new int[n];
+        var answerString = new string[n];
+        for (var i = 1; i <= answerInt.Length; i++)
+        {
+            answerInt[i - 1] = i;
+            if ((answerInt[i - 1] % 5) == 0 & (answerInt[i - 1] % 3) == 0)
+                answerString[i - 1] = "FizzBuzz";
+            else if ((answerInt[i - 1] % 5) == 0)
+                answerString[i - 1] = "Buzz";
+            else if ((answerInt[i - 1] % 3) == 0)
+                answerString[i - 1] = "Fizz";
+            else
+                answerString[i - 1] = i.ToString();
+        }
+        
+        return answerString;
+    }
+
+
+    private static void Main()
+    {
+        string[] answer = FizzBuzz(15);
+        foreach (var t in answer) Console.Write($"{t}, ");
+    }
+}
